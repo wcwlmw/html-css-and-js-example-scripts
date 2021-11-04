eb6b9881c83deaf438eca3de9e9051e08dc008f5
winnerwannabe <70723880+winnerwannabe@users.noreply.github.com>
2021-11-04 17:20:22 +0000

    added notations downloads and a bit of styling.

--- password.js
+++ password.js
@@ -1,9 +1,9 @@
 alert("creates popup");
-var answer = prompt("asks a question. (anwser hi)");
-if (answer == "hi"){//sets an anwser var is a variable
+var answer = prompt("asks a question. (anwser hi)");//sets an anwser var is a variable
+if (answer == "hi"){
   alert("You have passed. I will let you in!");//if they anwser corectly
 }
-else{//if they are wrong
+else{//if awnser is not in corospondince to the if statement
   alert("you were supposed to say hi... BEGONE!");
   window.location.href = "https://coverager.com/introducing-limu-emu-and-doug/"
 }//sends you to a link
